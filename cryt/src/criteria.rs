d0faf74798ea2bf79413772b4765e645c61db31c
Nicolas Schmidt <nfschmidt@gmail.com>
2017-08-20 17:15:57 -0300

    refactored xor module with structs and methods for different attacks

--- cryt/src/criteria.rs
+++ cryt/src/criteria.rs
@@ -1,6 +1,8 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
 
+pub type BytesCriterion = Fn(&[u8]) -> f32;
+
 const TEXT_BYTES: [u8; 59] = ['a' as u8, 'b' as u8, 'c' as u8, 'd' as u8, 'e' as u8, 'f' as u8, 'g' as u8, 'h' as u8, 'i' as u8, 'j' as u8, 'k' as u8, 'l' as u8, 'm' as u8, 'n' as u8, 'o' as u8, 'p' as u8, 'q' as u8, 'r' as u8, 's' as u8, 't' as u8, 'u' as u8, 'v' as u8, 'w' as u8, 'x' as u8, 'y' as u8, 'z' as u8, 'A' as u8, 'B' as u8, 'C' as u8, 'D' as u8, 'E' as u8, 'F' as u8, 'G' as u8, 'H' as u8, 'I' as u8, 'J' as u8, 'K' as u8, 'L' as u8, 'M' as u8, 'N' as u8, 'O' as u8, 'P' as u8, 'Q' as u8, 'R' as u8, 'S' as u8, 'T' as u8, 'U' as u8, 'V' as u8, 'W' as u8, 'X' as u8, 'Y' as u8, 'Z' as u8, ' ' as u8, ',' as u8, '.' as u8, '\'' as u8, '!' as u8, ';' as u8, ':' as u8];
 
 pub fn printable_bytes(bytes: &[u8]) -> f32 {
